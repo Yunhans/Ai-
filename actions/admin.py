e907f06ae863bc5deca1f09d3223a6c186d3025a
Yunhans <hugo52323@yahoo.com>
2022-11-16 02:44:37 +0800

    admin

--- actions/admin.py
+++ actions/admin.py
@@ -2,9 +2,7 @@ from line_chatbot_api import *
 
 def collect(event,answer):
     messages=[]
-    messages.append(TextSendMessage(text='管理員功能：收集樣本',quick_reply=QuickReply(items=[
-                                                                QuickReplyButton(action=URIAction(label="打開白板寫寫看", uri='https://liff.line.me/1657646010-mWYvBkxr')),
-                                                                ])))
+    messages.append(TextSendMessage(text='管理員功能：收集樣本',quick_reply=QuickReply(items=[QuickReplyButton(action=URIAction(label="打開白板寫寫看", uri='https://liff.line.me/1657646010-mWYvBkxr'))])))
     line_bot_api.reply_message(event.reply_token, messages)
     answer='collect'
     return answer
